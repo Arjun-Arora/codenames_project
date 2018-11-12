3f6b10fcbbae3ba61106181d5c3135ecdb690fb5
Arjun Arora <aarora52@stanford.edu>
2018-11-11 22:13:11 -0800

    created Tests and updated word_list
    created test to make sure utils.py is working
    
    Also updated word_list so it will work natively with trained google model from gensim

--- src/utils.py
+++ src/utils.py
@@ -9,11 +9,11 @@ def findNearestWord(corpus,model,vector):
 		model: this is the gensim word2vec model we're using for this function
 		vector: numpy array representing a chosen word2vecVector 
 
-	Output: String word that is nearest to word2vecVector from corpus (L2)
+	Output: String word that is nearest to word2vecVector from corpus (cosine distance)
 	'''
 
-	vecCorpus = sc.asarray([model.wv(word) for word in corpus])
-	minIndex = sc.argmin(sc.linalg.norm(vecCorpus-vector),axis = 0) #minimize across words not across vector length
+	vecCorpus = sp.asarray([model.wv[word] for word in corpus])
+	minIndex = sp.argmin([sp.spatial.distance.cosine(vecCorpus[i],vector) for i in range(len(vecCorpus))],axis = 0) #minimize across words not across vector length
 	return corpus[minIndex]
 
 
