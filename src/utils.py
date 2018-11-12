7a808338a24799b04bb7910f9614619dec94ad68
Arjun Arora <aarora52@stanford.edu>
2018-11-11 20:57:55 -0800

    created util.py and findNearestWord

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import scipy as sp 
+from gensim.models import Word2Vec
+
+def findNearestWord(corpus,model,vector): 
+	'''
+	find nearest word function: 
+	inputs:
+		corpus: List of words that the corpus we're using for this codenames game
+		model: this is the gensim word2vec model we're using for this function
+		vector: numpy array representing a chosen word2vecVector 
+
+	Output: String word that is nearest to word2vecVector from corpus (L2)
+	'''
+
+	vecCorpus = sc.asarray([model.wv(word) for word in corpus])
+	minIndex = sc.argmin(vecCorpus-vector,axis = 0) #minimize across words not across vector length dim
+	return corpus[minIndex]
+
+
