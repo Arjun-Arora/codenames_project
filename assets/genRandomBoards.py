cb3836fc80f266ee55c71be21c668184354f18bc
Heidi <heidichen7@gmail.com>
2018-11-14 14:44:52 -0800

    convert json to torch dict

---
+++ assets/genRandomBoards.py
@@ -0,0 +1,33 @@
+import random
+import copy
+import json
+# import argparse
+
+# parser = argparse.ArgumentParser(description='generate graph based on kNN')
+# parser.add_argument('--numboards', '-k', help='num boards to generate', type=int, default=5)
+# args = parser.parse_args()
+# nboards = args.numboards
+
+words = []
+f = open("assets/word_list.txt", "r")
+words = [line for line in f.read().split()]
+
+full_list = copy.deepcopy(words)
+nboards = 10
+
+blue_len = 9
+red_len = 8
+assassin_len = 1
+neutral_len = 7
+
+f2 = open("assets/board_list.json", "w")
+for i in range(nboards):
+	board_dic = {}
+	board = random.sample(words, 25)
+	board_dic['blue'] = board[ : blue_len]
+	board_dic['red'] = board[blue_len : blue_len+red_len]
+	board_dic['assassin'] = board[blue_len+red_len : blue_len+red_len+assassin_len]
+	board_dic['red'] += board[:-neutral_len] #for now, make the neutrals = red for simplicity
+
+	json.dump(board_dic, f2)
+	f2.write("\n")
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,18 @@
 import scipy as sp 
-from gensim.models import Word2Vec
+import torch
+import numpy as np 
+import gensim
+from gensim.models import Word2Vec 
+from gensim.test.utils import common_texts, get_tmpfile
+import collections
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='generate graph based on kNN')
+parser.add_argument('--boardfile', '-o', help='file of boards', default='assets/board_list.json')
+args = parser.parse_args()
+
+model = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin', binary=True, limit=500000)
 
 def findNearestWord(corpus,model,vector): 
 	'''
@@ -17,3 +30,26 @@ def findNearestWord(corpus,model,vector):
 	return corpus[minIndex]
 
 
+def convertToTorchDict(bdict):
+	w2v = collections.defaultdict(list)
+	for key in bdict:
+		for word in bdict[key]:
+			curr_word2vec = model.wv[word]
+			w2v[key].append(curr_word2vec)
+	tsr = {}
+	for key in w2v:
+		tsr[key] = torch.Tensor(w2v[key])
+	
+	for key in tsr:
+		print (key)
+		print (tsr[key].shape)
+	return tsr	
+
+
+
+with open(args.boardfile, 'r') as f:
+	dicts = []
+	for line in f:
+		board = json.loads(line)
+		tensorDict = convertToTorchDict(board) 
+		dicts.append(tensorDict)
