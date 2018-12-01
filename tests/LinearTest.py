f15c607dbec06b98b3920f1e626b09643013c2ff
Heidi <heidichen7@gmail.com>
2018-12-01 15:28:34 -0800

    adding kwordloss, in progress

--- tests/LinearTest.py
+++ tests/LinearTest.py
@@ -6,19 +6,19 @@ import linear_model
 import torch
 import gensim
 import numpy as np
-
+from tqdm import tqdm
 
 model_path = "basicmodel.pt"
-googlemodel = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin.gz', binary=True, limit=10000)
+googlemodel = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin', binary=True, limit=10000)
 
 #list of words
-kWords = 10
+kWords = 1000
 clues = googlemodel.index2word[:kWords]
 clues_w2v = [torch.Tensor(googlemodel.wv[word]) for word in clues]
 
-
 def BasicTest(): 
 	ListOfBoardDicts = utils.readBoards()
+	dev_boards = utils.readBoards("../assets/dev_board_list.json")
 	Input = [] 
 	for BoardDict in ListOfBoardDicts:
 		Input.append(torch.cat(list(BoardDict.values())))
@@ -27,65 +27,74 @@ def BasicTest():
 	# print(TensorInput.shape)
 	#data input,output size
 	numBoards, boardSz, w2v_sz = TensorInput.shape
-	TensorInput = TensorInput.view(numBoards,-1)
-	# N,D_in,D_out = TensorInput.shape
-	#hidden layer size
-	# H = 1000
+	TensorInput = TensorInput.view(numBoards,-1) #flatten out: numboards, sizeofboards x word2ve sz (2d)
 	model = linear_model.createSequential(boardSz, w2v_sz, k=kWords)
 	# print(model)
-	# criterion = linear_model.BasicLoss
+	print ("made model")
 	criterion = linear_model.BasicLoss
 
-	optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+	optimizer = torch.optim.Adam(model.parameters(), lr=1e-4) #alt to sgd
+	batchsz = 16 #consider loss of 16 boards at a time
 	for t in range(100):#epochs
-		output_vectors = model(TensorInput)
-		# print("model output shape: {}".format(output_vectors.shape))
-		loss = 0 
-		# print("output_vectors shape: {}".format(output_vectors.shape))
-		
-		for i in range(output_vectors.shape[0]):
-			# print(output_vectors[i,:].shape)
-			output_vector = output_vectors[i,:]
-			# print("output vector shape: {}".format(output_vector.shape))
-			# print("output_vector shape before input:{}".format(output_vector.shape))
-			loss += criterion(output_vector,ListOfBoardDicts[i], clues_w2v)
-		#print out loss
-		# print(t, loss.item())
+		print ("epoch " + str(t))
+		avg_loss = 0
+		for b in tqdm(range(int(numBoards/batchsz))):
+			output_vectors = model(TensorInput[b:b+batchsz,:])
+			# print("model output shape: {}".format(output_vectors.shape))
+			loss = 0 
+			# print("output_vectors shape: {}".format(output_vectors.shape))
+			
+			for i in range(output_vectors.shape[0]):
+				# print(output_vectors[i,:].shape)
+				output_vector = output_vectors[i,:]
+				# print("output vector shape: {}".format(output_vector.shape))
+				# print("output_vector shape before input:{}".format(output_vector.shape))
+				loss += criterion(output_vector,ListOfBoardDicts[i], clues_w2v)
+				avg_loss+=loss.item()
+			
+			#print out loss
+			#print(t, loss.item())
 
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+		
+		for board in dev_boards[:5]:
+			print (Inference(board, trained_model=model))
+		print ("average loss for epoch " + str(avg_loss / (numBoards/batchsz)))
 
 	torch.save(model, model_path)
 
-def Inference(board):
+def Inference(board, model_path=None, trained_model=None):
 
 	#unsqueezed to ensure that input is (1,ProbDistribution) because Softmax is done across the 1 dimension
 	Input = torch.unsqueeze(torch.cat(list(board.values())).view(-1),dim=0)
+	#load saved, fully trained model from model path
+	if model_path:
+		model = torch.load(model_path)
+	else:
+		model = trained_model
 
-	# print("Input shape: {}".format(Input.shape))
-	# googlemodel = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin', binary=True, limit=10000)
-	# print (googlemodel.index2word[-50:])
-	model = torch.load(model_path)
-	# torch.zero_grad()
 	with torch.no_grad():
 		model = model.eval()
 
 		output = model(Input)
 		# print (output)
 		nx = output.detach().numpy()
-		# max_prob = max(nx)
 		idx = np.argmax(nx)
 		# print ("clue: {}".format(clues[idx]))
 
 		criterion = linear_model.BasicLoss
 		print("eval criterion: {}".format(criterion(output, board, clues_w2v))) #also, output isn't the same as clue - clue is the nearest word
+		# board_words = utils.readBoardsWords()
+		# print("board")
+		# '{0:9} {0:8}'.format(board_words['blue'], board_words['red'])
 		return clues[idx]
 	
 
 
 BasicTest() 
-ListOfBoardDicts = utils.readBoards()
-for b in ListOfBoardDicts:
-	print(Inference(b))
-	break
+dev_boards = utils.readBoards("../assets/dev_board_list.json")
+for board in dev_boards[:5]:
+	Inference(board, model)
+
