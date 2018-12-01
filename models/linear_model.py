b0ce03f37057a6f66f097d3fd3cfa3db9e69dadf
Arjun Arora <aarora52@stanford.edu>
2018-12-01 13:50:11 -0800

    updated linear model and test to work
    corrected dimensionality issues

--- models/linear_model.py
+++ models/linear_model.py
@@ -5,10 +5,11 @@ import copy
 import numpy as np
 import itertools
 
-def createSequential(board_sz, w2v_sz, k, n=4,h=300):
+def createSequential(board_sz, w2v_sz, k=1000, n=4,h=200):
 	modules = []
 	activation = torch.nn.modules.activation.ReLU()
 	l = torch.nn.Linear(board_sz*w2v_sz,h)
+	modules.append(l)
 	for i in range(n):
 		l = torch.nn.Linear(h,h)
 		#linear layer, then nonlinear activation layer 
@@ -17,45 +18,45 @@ def createSequential(board_sz, w2v_sz, k, n=4,h=300):
 
 	l = torch.nn.Linear(h,k)
 	modules.append(l)
-	activation = torch.nn.Softmax() #generalization of sigmoid function - sums, then normalizes so probs sum to 1 
+	activation = torch.nn.Softmax(dim=1) #generalization of sigmoid function - sums, then normalizes so probs sum to 1
 	modules.append(activation)
-
 	sequential = torch.nn.Sequential(*modules)
 
 	return sequential
 
 def BasicLoss(probDistribution,boardDict,w2vDict,assassinWeight=2.0): #boardDict is word2vec Tensor version of board with labels 
-	probDistribution = torch.sum(probDistribution, dim=0) 
+	# probDistribution = torch.sum(probDistribution, dim=0) #why?
+	idx,prob= torch.argmax(probDistribution),torch.max(probDistribution)
 	# nx = probDistribution.detach().numpy()
 	# max_prob = max(nx)
 	# idx = np.where(nx == max_prob)[0][0]
 	# print (probDistribution)
 	completeLoss = 0
-	for idx, prob in enumerate(probDistribution):
-		outputVector = w2vDict[idx]
-		sumLoss = 0 
-		for key in boardDict:
-			currLoss = 0
-			if(key == 'blue'):
-				blueTensor = boardDict[key]
-				for word2vec in blueTensor:
-					#print("word2vecShape: {},outputVectorShape:{}".format(word2vec.shape,outputVector.shape))
-					#for every individual word2vec vector, sum cosine similarity
-					currLoss += 1 - F.cosine_similarity(word2vec,outputVector,dim=0) #cosine dist = 1-cosine_similarity
-				currLoss = -torch.mean(currLoss)
-			elif(key == 'red'):
-				redTensor = boardDict[key]
-				for word2vec in redTensor:
-				 	#for every individual word2vec vector, sum cosine similarity
-				 	currLoss += 1 - F.cosine_similarity(word2vec,outputVector,dim=0) #cosine dist = 1-cosine_similarity
-				currLoss = torch.mean(currLoss)
-			elif(key =='assassin'):
-				assassinVector = boardDict[key] #should be 1,len(word2Vec)
-				currLoss = (torch.mean(assassinWeight*(1-F.cosine_similarity(assassinVector,outputVector,dim=0))))
-			# print(currLoss.shape)
-			sumLoss += currLoss
-		
-		completeLoss += prob * sumLoss
+	# for idx, prob in enumerate(probDistribution):
+	outputVector = w2vDict[idx]
+	sumLoss = 0
+	for key in boardDict:
+		currLoss = 0
+		if(key == 'blue'):
+			blueTensor = boardDict[key]
+			for word2vec in blueTensor:
+				#print("word2vecShape: {},outputVectorShape:{}".format(word2vec.shape,outputVector.shape))
+				#for every individual word2vec vector, sum cosine similarity
+				currLoss += 1 - F.cosine_similarity(word2vec,outputVector,dim=0) #cosine dist = 1-cosine_similarity
+			currLoss = -torch.mean(currLoss)
+		elif(key == 'red'):
+			redTensor = boardDict[key]
+			for word2vec in redTensor:
+				#for every individual word2vec vector, sum cosine similarity
+				currLoss += 1 - F.cosine_similarity(word2vec,outputVector,dim=0) #cosine dist = 1-cosine_similarity
+			currLoss = torch.mean(currLoss)
+		elif(key =='assassin'):
+			assassinVector = boardDict[key] #should be 1,len(word2Vec)
+			currLoss = (torch.mean(assassinWeight*(1-F.cosine_similarity(assassinVector,outputVector,dim=0))))
+		# print(currLoss.shape)
+		sumLoss += currLoss
+
+	completeLoss += prob * sumLoss
 	# print (sumLoss)
 	# print (completeLoss)
 	# print ("\n\n\n\n\n")
--- tests/LinearTest.py
+++ tests/LinearTest.py
@@ -7,9 +7,9 @@ import torch
 import gensim
 import numpy as np
 
+
 model_path = "basicmodel.pt"
-model_path = "basicmodel.pt"
-googlemodel = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin', binary=True, limit=10000)
+googlemodel = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin.gz', binary=True, limit=10000)
 
 #list of words
 kWords = 10
@@ -26,28 +26,31 @@ def BasicTest():
 	TensorInput = torch.stack(Input) #(NumofBoards,sizeofBoards,word2vec_size)
 	# print(TensorInput.shape)
 	#data input,output size
-	numBoards, boardSz, w2v_sz = TensorInput.shape 
+	numBoards, boardSz, w2v_sz = TensorInput.shape
+	TensorInput = TensorInput.view(numBoards,-1)
 	# N,D_in,D_out = TensorInput.shape
 	#hidden layer size
 	# H = 1000
-
-	model = linear_model.createSequential(boardSz, w2v_sz, kWords)
+	model = linear_model.createSequential(boardSz, w2v_sz, k=kWords)
+	# print(model)
 	# criterion = linear_model.BasicLoss
 	criterion = linear_model.BasicLoss
 
 	optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 	for t in range(100):#epochs
 		output_vectors = model(TensorInput)
+		# print("model output shape: {}".format(output_vectors.shape))
 		loss = 0 
 		# print("output_vectors shape: {}".format(output_vectors.shape))
 		
 		for i in range(output_vectors.shape[0]):
 			# print(output_vectors[i,:].shape)
 			output_vector = output_vectors[i,:]
+			# print("output vector shape: {}".format(output_vector.shape))
 			# print("output_vector shape before input:{}".format(output_vector.shape))
 			loss += criterion(output_vector,ListOfBoardDicts[i], clues_w2v)
 		#print out loss
-		print(t, loss.item())
+		# print(t, loss.item())
 
 		optimizer.zero_grad()
 		loss.backward()
@@ -57,29 +60,32 @@ def BasicTest():
 
 def Inference(board):
 
-	Input = torch.cat(list(board.values())).unsqueeze(dim=0)
+	#unsqueezed to ensure that input is (1,ProbDistribution) because Softmax is done across the 1 dimension
+	Input = torch.unsqueeze(torch.cat(list(board.values())).view(-1),dim=0)
+
+	# print("Input shape: {}".format(Input.shape))
 	# googlemodel = gensim.models.KeyedVectors.load_word2vec_format('../assets/GoogleNews-vectors-negative300.bin/GoogleNews-vectors-negative300.bin', binary=True, limit=10000)
 	# print (googlemodel.index2word[-50:])
 	model = torch.load(model_path)
 	# torch.zero_grad()
-	model = model.eval()
+	with torch.no_grad():
+		model = model.eval()
 
-	output = model(Input)
-	print (output)
+		output = model(Input)
+		# print (output)
+		nx = output.detach().numpy()
+		# max_prob = max(nx)
+		idx = np.argmax(nx)
+		# print ("clue: {}".format(clues[idx]))
 
-	output = torch.sum(output, dim=0) 
-	nx = output.detach().numpy()
-	max_prob = max(nx)
-	idx = np.where(nx == max_prob)[0][0]
-	print (clues[idx])
-
-	criterion = linear_model.BasicLoss
-	print (criterion(output, board, clues_w2v)) #also, output isn't the same as clue - clue is the nearest word
+		criterion = linear_model.BasicLoss
+		print("eval criterion: {}".format(criterion(output, board, clues_w2v))) #also, output isn't the same as clue - clue is the nearest word
+		return clues[idx]
 	
 
 
 BasicTest() 
 ListOfBoardDicts = utils.readBoards()
 for b in ListOfBoardDicts:
-	print (Inference(b))
+	print(Inference(b))
 	break
